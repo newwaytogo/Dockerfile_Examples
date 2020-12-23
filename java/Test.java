576b3270f70fe7c5b6ee40d79a53eef18fcace02
newwaytogo <64739222+newwaytogo@users.noreply.github.com>
2020-12-23 23:09:58 +0800

    Update Test.java

--- java/Test.java
+++ java/Test.java
@@ -1,1 +1,14 @@
-package
+package wayto;
+
+public class Test {
+    public static void main(String args[]) {
+        while (true) {
+            System.out.println(System.currentTimeMillis() / 1000L);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptException e) {
+                break;
+            }
+        }
+    }
+}
